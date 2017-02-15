951751c9716f55940f2e12d0e0f4e58ffd587f7d
guanweitao <gwt9502@163.com>
2017-02-15 18:46:53 +0800

    'demo2-15'

---
+++ demo 2-15/src/components/News.vue
@@ -0,0 +1,55 @@
+<template>
+		<!--中间新闻内容部分-->
+		<div class="news-content">
+			<span class="news-title">这是一个长长的新闻标题</span>
+			<span class="news-date">2016-11-20</span>
+			<span class="news-article">
+				这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容
+				这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容
+				这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容
+				这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容这里是新闻内容。
+			</span>
+		</div>
+</template>
+
+<script>
+export default {
+  name: 'news',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.news-content{
+width:960px;
+margin:0 auto;
+}
+.news-title{
+width:100%;
+color:rgb(1,82,144);
+display: inline-block;
+text-align: center;
+font-size: 25px;
+margin-top: 40px;
+}
+.news-date{
+width:954px;
+padding-right: 6px;
+text-align: right;
+display: inline-block;
+color:rgb(185,185,185);
+}
+.news-article{
+text-indent: 2rem;
+line-height: 25px;
+text-align: left;
+display: block;
+color:rgb(185,185,185);
+}
+
+</style>
---
+++ demo 2-15/src/main.js
@@ -0,0 +1,47 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import index from 'components/index2'
+import Notice from 'components/Notice'
+import Square from 'components/Project-square'
+import Gwfc from 'components/Gwfc'
+import download from 'components/download'
+import vueTap from 'v-tap'
+// import fastclick from 'fastclick'
+import Vuex from 'vuex'
+
+Vue.use(vueTap)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+/* 控制路由 */
+const router = new VueRouter({
+  routes: [{
+    path: '/index',
+    component: index
+  }, {
+    path: '/notice',
+    component: Notice
+  }, {
+    path: '/square',
+    component: Square
+  }, {
+    path: '/gwfc',
+    component: Gwfc
+  }, {
+    path: '/download',
+    component: download
+  }]
+})
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+}).$mount('#app')
+
+/* 默认跳转 */
+router.push('index')
